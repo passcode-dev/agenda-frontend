ce83546b7b5a4d9d20b966eb660524c95ad3a2b0
Rodrigofroes <craftland262@gmail.com>
2025-01-08 23:05:49 -0300

    Feat: Adicionando ContextProvider

--- app/layout.js
+++ app/layout.js
@@ -1,37 +1,35 @@
+"use client";
 import { League_Spartan } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { Sidebar } from "@/components/layout/sidebar";
+import { UserProvider } from "./context/userContext";
 
 const league = League_Spartan({
   subsets: ["latin"],
 })
 
-export const metadata = {
-  title: "AulaFácil",
-  description: "AulaFácil é uma plataforma de agendameto de aulas online",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          league.className
-        )}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+    <UserProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            league.className
+          )}
         >
-          {children}
-          <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </body>
+      </html>
+    </UserProvider>
   );
 }
