3af5b7cf51bd6dff329326bc3fc51bb4296c6bb6
Rodrigofroes <craftland262@gmail.com>
2025-01-09 11:39:53 -0300

    Feat: Atualizando importação e layout da pagina

--- app/admin/alunos/page.js
+++ app/admin/alunos/page.js
@@ -1,95 +1,103 @@
 "use client";
 import Link from "next/link";
-import { ArrowLeft, ArrowLeftCircle, Filter, Pencil, Trash2 } from "lucide-react";
+import { ArrowLeft, Pencil, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { Spinner } from "@/components/ui/spinner";
 import AlunoService from "@/lib/service/alunoService";
-import { useRouter } from "next/navigation";
 import Back from "@/components/back";
 import Table from "@/components/tables/Tables";
 import { PaginationUI } from "@/components/paginationCustom";
 import FilterGroup from "@/components/Filters/FilterGroup";
 import FilterModal from "@/components/Filters/FilterModal";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Alunos() {
-    const [loading, setLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 10;
+    const [loading, setLoading] = useState(false);
     const [alunos, setAlunos] = useState([]);
+    const [totalPage, setTotalPage] = useState(3);
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const { toast } = useToast();
 
+    const currentPage = Number(searchParams.get("page")) || 1
 
     const filterSchema = [
-        {
-            name: 'Data de Nascimento',
-            Value: <input />
-        },
-        {
-            name: 'Telefone',
-
-        },
-        {
-            name: 'CEP',
-        }
+        { name: "Data de Nascimento", Value: <input /> },
+        { name: "Telefone" },
+        { name: "CEP" },
     ];
 
-
-
     const columns = [
+        { headerName: "#", field: "id" },
         { headerName: "Nome", field: "name" },
         { headerName: "Telefone", field: "phone_number" },
         { headerName: "RG", field: "rg" },
         { headerName: "CPF", field: "cpf" },
-        { headerName: "Data de Nascimento", field: "birth_date", renderCell: ({ row }) => new Date(row.birth_date).toLocaleDateString("pt-BR") },
         {
-            headerName: "Ações", field: "acoes", renderCell: (row) => (
+            headerName: "Data de Nascimento",
+            field: "birth_date",
+            renderCell: ({ row }) =>
+                new Date(row.birth_date).toLocaleDateString("pt-BR"),
+        },
+        {
+            headerName: "Ações",
+            field: "acoes",
+            renderCell: (params) => (
                 <div className="flex justify-center gap-3">
-                    <Button size="sm" onClick={() => editarAluno(row.id)}>
+                    <Button size="sm" onClick={() => editarAluno(params.row.id)}>
                         <Pencil className="w-4 h-4" />
                     </Button>
-                    <Button size="sm" onClick={() => deletarAluno(row.id)}>
+                    <Button size="sm" onClick={() => deletarAluno(params.row.id)}>
                         <Trash2 className="w-4 h-4" />
                     </Button>
                 </div>
-            )
-        }
+            ),
+        },
     ];
 
-
-    const fetchAlunos = async () => {
+    const fetchAlunos = async (page) => {
         setLoading(true);
-        const alunoService = await new AlunoService();
-        const alunos = await alunoService.alunos();
-        if (alunos.length > 0) {
-            setLoading(false);
-            setAlunos(alunos);
-        } else {
-            setLoading(false);
-        }
+        const alunoService = new AlunoService();
+        const alunos = await alunoService.alunos(page);
+        setAlunos(alunos);
+        setLoading(false);
     };
 
     useEffect(() => {
-        fetchAlunos();
-    }, []);
+        fetchAlunos(currentPage);
+    }, [currentPage]);
 
     const editarAluno = (id) => {
         router.push(`/admin/alunos/editar/${id}`);
     };
 
-    const deletarAluno = (id) => {
+    const deletarAluno = async (id) => {
         const confirm = window.confirm("Deseja realmente deletar este aluno?");
         if (!confirm) return;
 
-        console.log("Deletar aluno", id);
+        const alunoService = new AlunoService();
+        const deletar = await alunoService.deletarAluno(id);
+        if (!deletar) {
+            return toast({
+                title: "Erro",
+                description: "Erro ao deletar aluno, tente novamente.",
+                variant: "destructive",
+            });
+        }
+
+        fetchAlunos(currentPage);
+
+        toast({
+            title: "Sucesso",
+            description: "Aluno deletado com sucesso.",
+        });
     };
 
-    const paginationData = alunos.slice(
-        (currentPage - 1) * itemsPerPage,
-        currentPage * itemsPerPage
-    );
-
-    const totalPages = Math.ceil(alunos.length / itemsPerPage);
+    const handlePageChange = (page) => {
+        fetchAlunos(page);
+    };
 
     return (
         <div className="container max-w-4xl justify-center items-center mx-auto p-6">
@@ -100,22 +108,14 @@ export default function Alunos() {
             </div>
             <div className="mb-8 flex justify-between items-center">
                 <div>
-                    <h1 className="mt-4 text-3xl font-bold">
-                        Alunos
-                    </h1>
-                    <p className="text-muted-foreground">
-                        Lista de alunos cadastrados
-                    </p>
+                    <h1 className="mt-4 text-3xl font-bold">Alunos</h1>
+                    <p className="text-muted-foreground">Lista de alunos cadastrados</p>
                 </div>
                 <div>
                     <Link href="/admin/alunos/novo">
-                        <Button className="px-4 py-2 rounded mt-4">
-                            Novo Aluno
-                        </Button>
+                        <Button className="px-4 py-2 rounded mt-4">Novo Aluno</Button>
                     </Link>
-                    <FilterModal
-                        filterSchema={filterSchema}
-                    />
+                    <FilterModal filterSchema={filterSchema} />
                 </div>
             </div>
             <div className="mt-8">
@@ -123,31 +123,20 @@ export default function Alunos() {
                     <div className="flex justify-center items-center h-64">
                         <Spinner message="Carregando..." />
                     </div>
-                ) : (
-                    alunos.length > 0 ? (
-                        <>
-                            <FilterGroup
-                                filterSchema={filterSchema}
+                ) : alunos.length >= 0 ? (
+                    <>
+                        <FilterGroup filterSchema={filterSchema} />
+                        <Table data={alunos} columns={columns} />
+                        <div className="mt-4 flex justify-end items-center">
+                            <PaginationUI
+                                totalPage={totalPage}
+                                onPageChange={handlePageChange}
                             />
-                            <Table
-                                data={paginationData}
-                                columns={columns}
-                            />
-                            <div className="mt-4 flex justify-end items-center">
-                                <PaginationUI
-                                    currentPage={currentPage}
-                                    onPageChange={setCurrentPage}
-                                    totalPages={totalPages}
-                                />
-                            </div>
-                        </>
-                    ) : (
-                        <div className="flex justify-center items-center h-64">
-                            <p>Nenhum aluno cadastrado.</p>
                         </div>
-                    )
-                )}
+                    </>
+                ) : null
+                }
             </div>
-        </div >
+        </div>
     );
 }
