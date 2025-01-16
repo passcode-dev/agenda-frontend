7a6df5008a1ec62d7b759e9e63749ef64807c6eb
Rodrigofroes <craftland262@gmail.com>
2025-01-15 22:22:02 -0300

    Feat: Criação do componente

---
+++ components/alert.jsx
@@ -0,0 +1,28 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export function AlertDialogUI({ title, description, showDialog, setShowDialog, onConfirm }) {
+    return (
+        <AlertDialog open={showDialog} onOpenChange={setShowDialog}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>{title}</AlertDialogTitle>
+                    <AlertDialogDescription>{description}</AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel onClick={() => setShowDialog(false)}>Cancelar</AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>Confirmar</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+}
