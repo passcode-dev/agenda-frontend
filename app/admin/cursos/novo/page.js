51382ed6ac7b721444690ffcea16dfabbc1f4c57
RodrigoFroes <craftland262@gmail.com>
2025-01-30 22:06:26 -0300

    Feat: Criação do curso

---
+++ app/admin/cursos/novo/page.js
@@ -0,0 +1,103 @@
+"use client";
+import { ArrowLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Back from "@/components/back";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { zodCurso } from "@/lib/schemas/zod";
+import { useEffect, useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+import TurmaService from "@/lib/service/turmaService";
+import GenericSelectForm from "@/components/forms/genericoForm";
+import CursoService from "@/lib/service/cursoService";
+
+export default function Novo() {
+    const [loading, setLoading] = useState(false);
+    const [turmaCurso, setTurmaCurso] = useState([]);
+    const { register, reset, handleSubmit, setValue, formState: { errors } } = useForm({
+        resolver: zodResolver(zodCurso),
+    });
+    const { toast } = useToast();
+    const router = useRouter();
+    const [turmas, setTurmas] = useState([]);
+
+    const onSubmit = async (data) => {
+        setLoading(true);
+        let obj = {
+            ...data,
+            classes: turmaCurso.map((turma) => {
+                return { id: turma.id }
+            }),
+        }
+        const cursoService = new CursoService();
+        const cursos = await cursoService.cadastrarCurso(obj);
+        if (cursos.status === "success") {
+            setLoading(false);
+            toast({
+                title: "Curso cadastrada com sucesso!",
+                description: cursos.message,
+                status: "success",
+            });
+            return router.push("/admin/cursos");
+        };
+        setLoading(false);
+        return toast({
+            title: "Erro ao cadastrar o curso!",
+            description: cursos.data.details,
+            status: "error",
+        });
+    }
+
+    const fetchTurmas = async (page) => {
+        const turmaService = new TurmaService();
+        const turmas = await turmaService.Turmas(page);
+        setTurmas(turmas.data.classes);
+    };
+
+    useEffect(() => {
+        fetchTurmas();
+    }, []);
+
+    return (
+        <div className="container max-w-4xl justify-center items-center mx-auto p-6">
+            <div className="mb-8">
+                <div className="flex items-center gap-2">
+                    <Back icon={<ArrowLeft className="h-4 w-4" />} text="Voltar" href="/admin/cursos" />
+                </div>
+            </div>
+            <div className="mb-8 flex justify-between items-center">
+                <div>
+                    <h1 className="mt-4 text-3xl font-bold">
+                        Novo Curso
+                    </h1>
+                    <p className="text-muted-foreground">
+                        Cadastre uma novo curso
+                    </p>
+                </div>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <GenericSelectForm
+                        register={register}
+                        errors={errors}
+                        setValue={setValue}
+                        items={turmas}
+                        setSelectedItems={setTurmaCurso}
+                        selectedItems={turmaCurso}
+                        label="Curso"
+                        placeholder="Selecione uma turma..."
+                        itemName="Classes"
+                        searchPlaceholder="Buscar turma..."
+                        noItemsMessage="Nenhum turma encontrada."
+                        labelSelect="Turmas"
+                    />
+                    <Button type="submit" className="mt-4 w-24" disabled={loading}>
+                        {loading ? <Spinner className="text-gray-800" /> : "Cadastrar"}
+                    </Button>
+                </form>
+            </div>
+        </div >
+    );
+}
---
+++ app/admin/cursos/page.js
@@ -0,0 +1,150 @@
+"use client";
+import FilterGroup from "@/components/Filters/FilterGroup";
+import FilterModal from "@/components/Filters/FilterModal";
+import { PaginationUI } from "@/components/paginationCustom";
+import { Spinner } from "@/components/ui/spinner";
+import { Pencil, Trash2 } from "lucide-react";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
+import Tables from "@/components/tables/Tables";
+import { AlertDialogUI } from "@/components/alert";
+import CursoService from "@/lib/service/cursoService";
+import { Badge } from "@/components/ui/badge";
+
+export default function Cursos() {
+    const [loading, setLoading] = useState(false);
+    const [turmas, setTurmas] = useState([]);
+    const [showDialog, setShowDialog] = useState(false);
+    const [confirmCallback, setConfirmCallback] = useState(null);
+    const [totalPage, setTotalPage] = useState(0);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const { toast } = useToast();
+
+    const currentPage = Number(searchParams.get("page")) || 1
+    const filterSchema = [
+        { name: "Nome" },
+    ];
+
+    const columns = [
+        { headerName: "#", field: "id" },
+        { headerName: "Curso", field: "name" },
+        {
+            headerName: "Turma", field: "name", renderCell: (params) => (
+                <div className="flex flex-wrap  gap-1 justify-center">
+                    {params.row.Classes.map((turma, index) => (
+                        <Badge className="p-2"
+                            key={index}
+                        >
+                            {turma.name}
+                        </Badge>
+                    ))}
+                </div>
+            ),
+        },
+        {
+            headerName: "Ações",
+            field: "acoes",
+            renderCell: (params) => (
+                <div className="flex justify-center gap-3">
+                    <Button size="sm" onClick={() => editarMateria(params.row.id)}>
+                        <Pencil className="w-4 h-4" />
+                    </Button>
+                    <Button size="sm" onClick={() => deletarCurso(params.row.id)}>
+                        <Trash2 className="w-4 h-4" />
+                    </Button>
+                </div>
+            ),
+        },
+    ];
+
+    const fetchCursos = async (page) => {
+        setLoading(true);
+        const cursoService = new CursoService();
+        const cursos = await cursoService.Cursos(page);
+        setTurmas(cursos.data.courses);
+        setTotalPage(Math.ceil(cursos.data.total_records / 10));
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchCursos(currentPage);
+    }, [currentPage]);
+
+    const editarMateria = (id) => {
+        router.push(`/admin/cursos/editar/${id}`);
+    };
+
+    const deletarCurso = async (id) => {
+        setShowDialog(true);
+        setConfirmCallback(() => async () => {
+            const cursoService = new CursoService();
+            const deletar = await cursoService.deletarCurso(id);
+            if (deletar.status == "success") {
+                fetchCursos(currentPage);
+                setShowDialog(false);
+                return toast({
+                    title: "Sucesso",
+                    description: deletar.message,
+                });
+            }
+            fetchCursos(currentPage);
+            setShowDialog(false);
+            return toast({
+                title: "Erro",
+                description: deletar.message,
+            });
+        });
+    };
+
+    const handlePageChange = (page) => {
+        fetchCursos(page);
+    };
+
+    return (
+        <div className="container max-w-4xl justify-center items-center mx-auto p-6">
+            <AlertDialogUI
+                title="Confirmação de exclusão"
+                description="Deseja realmente deletar esse curso ?"
+                showDialog={showDialog}
+                setShowDialog={setShowDialog}
+                onConfirm={confirmCallback}
+            />
+            <div className="mb-8 flex justify-between items-center">
+                <div>
+                    <h1 className="mt-4 text-3xl font-bold">Cursos</h1>
+                    <p className="text-muted-foreground">Lista de cursos cadastrados</p>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-2">
+                    <FilterModal filterSchema={filterSchema} />
+                    <Link className="flex items-center justify-center" href="/admin/cursos/novo">
+                        <Button className="px-4">Novo Curso</Button>
+                    </Link>
+                </div>
+            </div>
+            <div className="mt-8">
+                {loading ? (
+                    <div className="flex justify-center items-center h-64">
+                        <Spinner message="Carregando..." />
+                    </div>
+                ) : turmas.length >= 0 ? (
+                    <>
+                        <FilterGroup filterSchema={filterSchema} />
+                        <Tables data={turmas} columns={columns} isSubjects={true} />
+                        <div className="mt-4 flex justify-end items-center">
+                            <PaginationUI
+                                totalPage={totalPage}
+                                onPageChange={handlePageChange}
+                            />
+                        </div>
+                    </>
+                ) : null
+                }
+            </div>
+        </div>
+    );
+}
