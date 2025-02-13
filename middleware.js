7bebed81022f9f302cac5bff25c1e80da2737c97
RodrigoFroes <craftland262@gmail.com>
2025-02-13 12:05:40 -0300

    Feat: Add middleware de rotas

--- middleware.js
+++ middleware.js
@@ -1,11 +1,15 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request) {
-    return NextResponse.redirect(new URL('/', request.url))
+    const token = request.cookies.get('Authorization');
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/auth', ],
-}
+    matcher: ['/admin/:path*',]
+};
