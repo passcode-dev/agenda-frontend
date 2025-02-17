6fef3adc9ea6c064b0aafe44963bbfebf5163ed2
Leonardo Valões <leovaloesnovaes@gmail.com>
2025-02-17 11:21:05 -0300

    Refactor: refatoração na tela de matérias

--- components/forms/classeForm.js
+++ components/forms/classeForm.js
@@ -3,22 +3,21 @@
 import InputWithMask from "../ui/inputWithMask";
 
 export default function ClasseForm({ classe, setClasseData }) {
+  const handleChange = (name, value) => {
+    setClasseData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleChange = (name, value) => {
-        setClasseData((prevState) => ({
-          ...prevState,
-          [name]: value,
-        }));
-      };
-
-      return (
-        <form className="space-y-4">
-          <InputWithMask
-            label="Nome"
-            name="name"
-            value={classe.name}
-            onChange={handleChange}
-          />
-        </form>
-      );
+  return (
+    <form className="space-y-4">
+      <InputWithMask
+        label="Nome"
+        name="name"
+        value={classe.name}
+        onChange={handleChange}
+      />
+    </form>
+  );
 }
--- components/forms/materiaForm.js
+++ components/forms/materiaForm.js
@@ -1,108 +1,25 @@
-"use client"
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
-import { Badge } from "@/components/ui/badge";
-import { X } from "lucide-react";
+"use client";
 
-export default function MateriaForm({ register, errors, setValue, professores, initialValues, setProfessoresMateria, professoresMateria }) {
-    const [searchTerm, setSearchTerm] = useState("");
+import { useCallback, useEffect } from "react";
+import InputWithMask from "../ui/inputWithMask";
 
-    useEffect(() => {
-        if (initialValues) {
-            setValue("name", initialValues.name);
-            setProfessoresMateria(initialValues.Teachers);
-        }
-    }, [initialValues, setValue]);
+export default function MateriaForm({ materia, setMateriaData }) {
+  const handleChange = (name, value) => {
+    setMateriaData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    const handleAdicionarProfessor = (professor) => {
-        const professorSelecionado = professores.find(
-            (prof) => prof.id === professor
-        );
-        if (
-            professorSelecionado &&
-            !professoresMateria.some((prof) => prof.id === professorSelecionado.id)
-        ) {
-            setProfessoresMateria((prev) => [...prev, professorSelecionado]);
-        }
-        setSearchTerm(""); 
-    };
-
-   
-    const filteredProfessores = searchTerm
-        ? professores.filter(professor =>
-            professor.name.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        : professores;
-
-    return (
-        <div>
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-                <div>
-                    <Label>Matéria</Label>
-                    <Input
-                        type="text"
-                        {...register("name")}
-                    />
-                    {errors.name && (<p className="text-red-500 text-sm">*{errors.name.message}</p>)}
-                </div>
-                <div>
-                    <Label>Professores</Label>
-                    <Select onValueChange={handleAdicionarProfessor}>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Selecione um professor..." />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <Input
-                                type="text"
-                                placeholder="Buscar professor..."
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                                className="p-2"
-                            />
-                            {filteredProfessores.length > 0 ? (
-                                filteredProfessores.map((professor) => (
-                                    <SelectItem key={professor.id} value={professor.id}>
-                                        {professor.name}
-                                    </SelectItem>
-                                ))
-                            ) : (
-                                <p className="p-2 text-gray-500">Nenhum professor encontrado.</p>
-                            )}
-                        </SelectContent>
-                    </Select>
-                </div>
-            </div>
-            <div className="flex flex-col flex-wrap gap-2 mt-4">
-                <Label>Professores adicionados</Label>
-                <div className="flex flex-wrap gap-2">
-                    {professoresMateria?.length > 0 ? (
-                        professoresMateria.map((professor) => (
-                            <Badge key={professor.id} className="flex items-center gap-2 justify-between p-2">
-                                <span>{professor.name}</span>
-                                <button
-                                    onClick={() =>
-                                        setProfessoresMateria((prev) =>
-                                            prev.filter((prof) => prof.id !== professor.id)
-                                        )
-                                    }
-                                >
-                                    <X size={16} />
-                                </button>
-                            </Badge>
-                        ))
-                    ) : (
-                        <p className="text-sm text-gray-800">Nenhum professor adicionado</p>
-                    )}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <form className="space-y-4">
+      {/* Input para o nome da matéria */}
+      <InputWithMask
+        label="Nome"
+        name="name"
+        value={materia.name}
+        onChange={handleChange}
+      />
+    </form>
+  );
 }
