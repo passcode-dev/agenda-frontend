80d713cacae7e229d740257633fed60a9e6a3997
Leonardo Valões <leovaloesnovaes@gmail.com>
2025-02-17 13:03:27 -0300

    refactor: refatoração na tela de professores

--- app/admin/professores/page.js
+++ app/admin/professores/page.js
@@ -1,167 +1,273 @@
 "use client";
-import Back from "@/components/back";
 import FilterGroup from "@/components/Filters/FilterGroup";
 import FilterModal from "@/components/Filters/FilterModal";
 import { PaginationUI } from "@/components/paginationCustom";
 import { Spinner } from "@/components/ui/spinner";
 import ProfessoresService from "@/lib/service/professoresService";
-import { ArrowLeft, Pencil, Trash2, UserRound, IdCard, Calendar } from "lucide-react";
-import Link from "next/link";
+import { Pencil, Trash2, UserRound, IdCard } from "lucide-react";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import Tables from "@/components/tables/Tables";
 import { AlertDialogUI } from "@/components/alert";
+import styled from "styled-components";
+import ProfessorForm from "@/components/forms/professorForm";
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
+  z-index: 99;
+`;
+
+const GenericModalContent = styled.div`
+  position: fixed;
+  top: 50px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  max-width: 1000px;
+  max-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+
+  opacity: 0;
+  transform: translateY(-20px);
+  animation: slideDown 0.3s ease-out forwards;
+
+  @keyframes slideDown {
+    from {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
 
 export default function Professores() {
-    const [loading, setLoading] = useState(false);
-    const [professores, setProfessores] = useState([]);
-    const [showDialog, setShowDialog] = useState(false);
-    const [confirmCallback, setConfirmCallback] = useState(null);
-    const [totalPage, setTotalPage] = useState(0);
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const { toast } = useToast();
+  const [loading, setLoading] = useState(false);
+  const [professores, setProfessores] = useState([]);
+  const [showDialog, setShowDialog] = useState(false);
+  const [confirmCallback, setConfirmCallback] = useState(null);
+  const [editProfessor, setEditProfessor] = useState(null);
+  const [novoProfessor, setNovoProfessor] = useState(null);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const searchParams = useSearchParams();
+  const { toast } = useToast();
 
+  const filterSchema = [
+    { name: "Nome", parameterName: "name", icon: <UserRound /> },
+    { name: "CPF", parameterName: "cpf", icon: <IdCard /> },
+  ];
 
-    const currentPage = Number(searchParams.get("page")) || 1
-    const filterSchema = [
-        { name: "Data de Nascimento", parameterName: "birth_date", icon: <Calendar />, },
-        { name: "Nome", parameterName: "name", icon: <UserRound /> },
-        { name: "CPF", parameterName: "cpf", icon: <IdCard />, },
-    ];
+  const filterSchema2 = [
+    { name: "Nome", parameterName: "name", icon: <UserRound /> },
+  ];
 
-    const filterSchema2 = [
-        { name: "Nome", parameterName: "name", icon: <UserRound /> },
-    ];
+  const columns = [
+    { headerName: "#", field: "id" },
+    { headerName: "Nome", field: "name" },
+    { headerName: "CPF", field: "cpf" },
+    {
+      headerName: "Data de Nascimento",
+      field: "BirthDate",
+      renderCell: (params) => {
+        const date = new Date(params.row.BirthDate);
+        return date.toLocaleDateString("pt-BR");
+      },
+    },
+    {
+      headerName: "Ações",
+      field: "acoes",
+      renderCell: (params) => (
+        <div className="flex justify-center gap-3">
+          <Button size="sm" onClick={(e) => editarProfessor(params.row, e)}>
+            <Pencil className="w-4 h-4" />
+          </Button>
+          <Button size="sm" onClick={(e) => deletarProfessor(params.row.id, e)}>
+            <Trash2 className="w-4 h-4" />
+          </Button>
+        </div>
+      ),
+    },
+  ];
 
+  const fetchProfessores = async (params) => {
+    setLoading(true);
+    const professorService = new ProfessoresService();
+    const professores = await professorService.Professores(params);
+    setHasNextPage(false);
+    if (professores?.data?.teachers?.length > 10) {
+      setHasNextPage(true);
+      professores.data.teachers.pop();
+    }
+    setProfessores(professores?.data?.teachers);
+    setLoading(false);
+  };
 
-    const columns = [
-        { headerName: "#", field: "id" },
-        { headerName: "Nome", field: "name" },
-        { headerName: "CPF", field: "cpf" },
-        {
-            headerName: "Data de Nascimento",
-            field: "BirthDate",
-            renderCell: (params) => {
-                const date = new Date(params.row.BirthDate);
-                return date.toLocaleDateString("pt-BR");
-            },
+  useEffect(() => {
+    fetchProfessores(searchParams.toString());
+  }, [searchParams]);
 
-        },
-        {
-            headerName: "Ações",
-            field: "acoes",
-            renderCell: (params) => (
-                <div className="flex justify-center gap-3">
-                    <Button size="sm" onClick={() => editarProfessor(params.row.id)}>
-                        <Pencil className="w-4 h-4" />
-                    </Button>
-                    <Button size="sm" onClick={() => deletarProfessor(params.row.id)}>
-                        <Trash2 className="w-4 h-4" />
-                    </Button>
-                </div>
-            ),
-        },
-    ];
+  const cadastrarProfessor = async (professor) => {
+    const professorService = new ProfessoresService();
+    const resultado = await professorService.cadastrarProfessor(professor);
 
-    const fetchProfessor = async (page) => {
-        setLoading(true);
-        const professorService = new ProfessoresService();
-        const professores = await professorService.Professores(searchParams);
-        setTotalPage(Math.ceil(professores.data.total_records / 10));
-        setProfessores(professores.data.teachers);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        fetchProfessor(currentPage);
-    }, [currentPage]);
-
-    const editarProfessor = (id) => {
-        router.push(`/admin/professores/editar/${id}`);
-    };
-
-    const deletarProfessor = async (id) => {
-        setShowDialog(true);
-        setConfirmCallback(() => async () => {
-            const professorService = new ProfessoresService();
-            const deletar = await professorService.deletarProfessor(id);
-            if (deletar.status == "success") {
-                setShowDialog(false);
-                fetchProfessor(currentPage);
-                return toast({
-                    title: "Sucesso",
-                    description: deletar.message,
-                    variant: "success"
-                });
-            }
-            setShowDialog(false);
-            fetchProfessor(currentPage);
-            return toast({
-                title: "Erro",
-                description: deletar.data.details,
-                variant: "destructive"
-            });
+    if (resultado.status === "success") {
+      setNovoProfessor(null); // Fechar o modal
+      fetchProfessores(searchParams.toString()); // Recarregar a lista de professores
+      toast({
+        title: "Sucesso",
+        description: "Professor cadastrado com sucesso",
+        variant: "success",
+      });
+    } else {
+      toast({
+        title: "Erro",
+        description: resultado?.data?.details,
+        variant: "destructive",
+      });
+    }
+  };
+  const deletarProfessor = async (id, e) => {
+    setShowDialog(true);
+    e.stopPropagation();
+    setConfirmCallback(() => async () => {
+      const professorService = new ProfessoresService();
+      const deletar = await professorService.deletarProfessor(id);
+      if (deletar.status == "success") {
+        fetchProfessores(searchParams.toString());
+        setShowDialog(false);
+        return toast({
+          title: "Sucesso",
+          description: deletar?.message,
+          variant: "success",
         });
-    };
+      } else {
+        return toast({
+          title: "Erro",
+          description: deletar?.data?.details,
+          variant: "destructive",
+        });
+      }
+    });
+  };
 
-    const handlePageChange = (page) => {
-        fetchProfessor(page); // Chama a função para buscar os dados da nova página
-    };
-
-    useEffect(() => {
-        fetchProfessor(currentPage); // Chama a função de busca com o `currentPage` da URL
-    }, [currentPage, searchParams]); // O useEffect será chamado sempre que `currentPage` mudar
-
-    useEffect(() => {
-        const params = new URLSearchParams();
-        params.set("page", currentPage);
-        router.push(`${window.location.pathname}?${params.toString()}`)
-    }, []);
-
-    return (
-        <div className="container max-w-4xl justify-center items-center mx-auto p-6">
-            <AlertDialogUI
-                title="Confirmação de exclusão"
-                description="Deseja realmente deletar este professor?"
-                showDialog={showDialog}
-                setShowDialog={setShowDialog}
-                onConfirm={confirmCallback}
-            />
-            <div className="mb-8 flex justify-between items-center">
-                <div>
-                    <h1 className="mt-4 text-3xl font-bold">Professores</h1>
-                    <p className="text-muted-foreground">Lista de professores cadastrados</p>
-                </div>
-                <div className="flex flex-row justify-center items-center gap-2">
-                    <FilterModal filterSchema={filterSchema} />
-                    <Link className="flex items-center justify-center" href="/admin/professores/novo">
-                        <Button className="px-4 ">Novo Professor</Button>
-                    </Link>
-                </div>
-            </div>
-            <div className="mt-8">
-                {loading ? (
-                    <div className="flex justify-center items-center h-64">
-                        <Spinner message="Carregando..." />
-                    </div>
-                ) : professores.length >= 0 ? (
-                    <>
-                        <FilterGroup filterSchema={filterSchema} />
-                        <Tables data={professores} columns={columns} />
-                        <div className="mt-4 flex justify-end items-center">
-                            <PaginationUI
-                                totalPage={totalPage}
-                                onPageChange={handlePageChange}
-                            />
-                        </div>
-                    </>
-                ) : null
-                }
-            </div>
-        </div >
+  const fetchEditarProfessor = async (professor) => {
+    const professorService = new ProfessoresService();
+    const editar = await professorService.editarProfessor(
+      professor.id,
+      editProfessor
     );
+    if (editar.status != "error") {
+      setEditProfessor(null);
+      fetchProfessores(searchParams.toString());
+      return toast({
+        title: "Sucesso",
+        description: "Professor editado com sucesso",
+        variant: "success",
+      });
+    } else {
+      return toast({
+        title: "Erro ao editar professor",
+        description: editar?.data?.details,
+        variant: "destructive",
+      });
+    }
+  };
+
+  const editarProfessor = (professor, e) => {
+    setEditProfessor(professor);
+    e.stopPropagation(); // Evita que o clique propague para a célula da tabela
+  };
+
+  return (
+    <>
+      {!!novoProfessor && (
+        <>
+          <Backdrop onClick={() => setNovoAluno(null)} />
+          <GenericModalContent>
+            <ProfessorForm
+              professor={novoProfessor}
+              setProfessorData={setNovoProfessor}
+            />
+            <div>
+              <button onClick={() => cadastrarProfessor(novoProfessor)}>
+                Salvar
+              </button>
+              <button onClick={() => cadastrarProfessor(null)}>Cancelar</button>
+            </div>
+          </GenericModalContent>
+        </>
+      )}
+      {!!editProfessor && (
+        <>
+          <Backdrop onClick={() => setEditProfessor(false)} />
+          <GenericModalContent>
+            <ProfessorForm
+              professor={editProfessor}
+              setProfessorData={setEditProfessor}
+            />
+            <div>
+              <button onClick={() => fetchEditarProfessor(editProfessor)}>
+                Salvar{" "}
+              </button>
+              <button onClick={() => setEditProfessor(null)}>Cancelar</button>
+            </div>
+          </GenericModalContent>
+        </>
+      )}
+      <div className="container max-w-4xl justify-center items-center mx-auto p-6">
+        <AlertDialogUI
+          title="Confirmação de exclusão"
+          description="Deseja realmente deletar este professor?"
+          showDialog={showDialog}
+          setShowDialog={setShowDialog}
+          onConfirm={confirmCallback}
+        />
+        <div className="mb-8 flex justify-between items-center">
+          <div>
+            <h1 className="mt-4 text-3xl font-bold">Professores</h1>
+            <p className="text-muted-foreground">
+              Lista de professores cadastrados
+            </p>
+          </div>
+          <div className="flex flex-row justify-center items-center gap-2">
+            <FilterModal filterSchema={filterSchema} />
+            <Button className="px-4" onClick={() => setNovoProfessor({})}>
+              Novo Professor
+            </Button>
+          </div>
+        </div>
+        <div className="mt-8">
+          {loading ? (
+            <div className="flex justify-center items-center h-64">
+              <Spinner message="Carregando..." />
+            </div>
+          ) : professores.length >= 0 ? (
+            <>
+              <FilterGroup filterSchema={filterSchema} />
+              <Tables data={professores} columns={columns} />
+              <div className="mt-4 flex justify-end items-center">
+                <PaginationUI hasNextPage={hasNextPage} />
+              </div>
+            </>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
 }
